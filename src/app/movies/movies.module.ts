a800840aee43690c2173ceea6785680d235c79b5
AndersonAndrad <anderson_andrade_@outlook.com>
2022-11-27 11:14:47 -0300

    link controler with service and movies service external api

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -1,11 +1,13 @@
 import { Global, Module } from '@nestjs/common'
 
+import { MovieApiService } from 'src/infra/api/movie.api'
 import { MovieService } from './movies.service'
 import { MoviesController } from './movies.controller'
 
 Global()
 @Module( {
   controllers: [MoviesController],
-  exports: [MovieService]
+  exports: [MovieService],
+  providers: [MovieService, MovieApiService],
 } )
 export class MovieModule {}
--- src/app/movies/movies.service.ts
+++ src/app/movies/movies.service.ts
@@ -1,1 +1,17 @@
-export class MovieService {}
+import { Injectable } from "@nestjs/common"
+import { MovieApiService } from "src/infra/api/movie.api"
+
+@Injectable()
+export class MovieService {
+  constructor ( private movieApi: MovieApiService ) {}
+
+  public async getMovies () {
+    const {movies} = await this.movieApi.getMovies()
+
+    return movies
+  }
+
+  public async getMovie ( movieId: string ) {
+    throw new Error( "Method not implemented" )
+  }
+}
