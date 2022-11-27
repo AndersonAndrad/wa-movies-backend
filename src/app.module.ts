21060746f503cb1b0c31500e3c87924852a4d1d2
AndersonAndrad <anderson_andrade_@outlook.com>
2022-11-27 11:43:37 -0300

    add dependence nestjs/config - and cretae part of env.example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { ConfigModule } from '@nestjs/config'
 import { Module } from '@nestjs/common'
 import { MovieModule } from './app/movies/movies.module'
 
 @Module( {
-  imports: [MovieModule],
+  imports: [
+    MovieModule,
+    ConfigModule.forRoot( {
+    isGlobal: true,
+    envFilePath: ['.env', 'variaveis.ini'],
+  } ),],
   controllers: [AppController],
   providers: [AppService]
 } )
