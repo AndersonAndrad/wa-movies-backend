98aaa19bac205ed123dbdd7573d6eedca381fbc0
AndersonAndrad <anderson_andrade_@outlook.com>
2022-11-27 12:03:29 -0300

    add dependence mongoose - create connection with database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,19 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
 import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
 import { MovieModule } from './app/movies/movies.module'
 
-@Module( {
+@Module({
   imports: [
     MovieModule,
-    ConfigModule.forRoot( {
-    isGlobal: true,
-    envFilePath: ['.env', 'variaveis.ini'],
-  } ),],
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', 'variaveis.ini']
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI)
+  ],
   controllers: [AppController],
   providers: [AppService]
-} )
+})
 export class AppModule {}
