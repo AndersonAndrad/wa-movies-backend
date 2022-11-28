fb1cd7eeaa0f399ddd8f5cd0afa1fd6032f9e3d2
AndersonAndrad <anderson_andrade_@outlook.com>
2022-11-28 18:52:54 -0300

    add axios according to the nestjs documentation

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -1,13 +1,15 @@
 import { Global, Module } from '@nestjs/common'
 
+import { HttpModule } from '@nestjs/axios'
 import { MovieApiService } from 'src/infra/api/movie.api'
 import { MovieService } from './movies.service'
 import { MoviesController } from './movies.controller'
 
 Global()
-@Module( {
+@Module({
+  imports: [HttpModule],
   controllers: [MoviesController],
   exports: [MovieService],
-  providers: [MovieService, MovieApiService],
-} )
+  providers: [MovieService, MovieApiService]
+})
 export class MovieModule {}
--- src/app/movies/movies.service.ts
+++ src/app/movies/movies.service.ts
@@ -1,17 +1,22 @@
-import { Injectable } from "@nestjs/common"
-import { MovieApiService } from "src/infra/api/movie.api"
+import { AxiosResponse } from 'axios'
+import { HttpService } from '@nestjs/axios'
+import { IMovie } from 'src/infra/data/interfaces/movie.interface'
+import { Injectable } from '@nestjs/common'
+import { Observable } from 'rxjs'
 
 @Injectable()
 export class MovieService {
-  constructor ( private movieApi: MovieApiService ) {}
+  constructor(private httpService: HttpService) {}
 
-  public async getMovies () {
-    const {movies} = await this.movieApi.getMovies()
+  public async getMovies() {
+    const observableMovie = this.httpService.get<
+      Observable<AxiosResponse<IMovie[]>>
+    >('https://ghibliapi.herokuapp.com/films')
 
-    return movies
+    observableMovie.subscribe(async ({ data }) => {})
   }
 
-  public async getMovie ( movieId: string ) {
-    throw new Error( "Method not implemented" )
+  public async getMovie(movieId: string) {
+    throw new Error('Method not implemented')
   }
 }
